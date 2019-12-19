da8bfa314a68b341c5bb178b85cf91721bc54973
SURYANARAYAN RATH <suryanarayanrath74@gmail.com>
2019-12-19 20:19:28 +0530

    Use StirngUtils and added dependency to Lang3

--- src/main/java/trv/javaprogramming/training/Application.java
+++ src/main/java/trv/javaprogramming/training/Application.java
@@ -1,8 +1,16 @@
 package trv.javaprogramming.training;
 
-import java.util.*;
+import java.util.List;
+import java.util.ArrayList;
+import org.apache.commons.lang3.StringUtils;
 
 public class Application {
+
+    public int countWords(String words){
+        String[] countwords = StringUtils.split(words,' ');
+        return (countwords == null) ? 0 : countwords.length;
+    }
+
     public void greet(){
         List<String> greetings = new ArrayList<>();
         greetings.add("Hello");
@@ -18,6 +26,9 @@ public class Application {
     // method main(): ALWAYS the APPLICATION entry point
     public static void main (String[] args) {
     	System.out.println ("Starting Application");
-	Application app = new Application();
+        Application app = new Application();
+        app.greet();
+        int countWords = app.countWords("Hi My Name is Surya");
+        System.out.println("WordCount is "+ countWords);
     }
 }
